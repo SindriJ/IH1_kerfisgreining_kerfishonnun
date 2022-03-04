e5f4f36e6556be223178e3fb7e33275a5eb155d8
SindriJ <sindrijon21@ru.is>
2022-03-04 21:44:39 +0000

    fyrsta test

---
+++ sample.py
@@ -0,0 +1,20 @@
+
+#Fizzbuzz prógram
+#Tekur inn int, skilar Buzz ef talan er deilanleg með 5, Fizz ef hún er deilanleg með 3. 
+#Ef talan er bæði deilanleg með 3 og 5 skilar fallið FizzBuzz
+#Ef talan deilist hvorki með 3 eða 5 þá skilar fallið tölunni til baka.
+
+
+def fizzbuzz(int):
+    if int %3 == 0 and int%5 == 0:
+        return "FizzBuzz"
+    else:
+        return None
+
+# prufa = fizzbuzz(12)
+# print (prufa)
+
+
+def func(x):
+    return x + 1
+
--- test_sample.py
+++ test_sample.py
@@ -1,7 +1,25 @@
+
+#Geri test fyrir hvert req.
+# 1. "FizzBuzz" if the input number is divisible by 5 and 3
+# 2. "Buzz" if the input number is divisible by 5
+# 3. "Fizz" if the input number is divisible by 3
+# 4. The input number itself if none of the other statements are true.
+
+
+from sample import fizzbuzz
+from sample import func
+
+def test_FizzBuzz():
+    for i in [15,30]:
+        assert fizzbuzz(i) == 'FizzBuzz'
+
+
+
+
 # content of test_sample.py
-def func(x):
-    return x + 1
+# def func(x):
+#     return x + 1
 
 
-def test_answer():
-    assert func(3) == 5
+# def test_answer():
+#     assert func(3) == 5
